e5483f2320fe692b6bc68ee9e827dd906dfb3e11
DarkMattrMaestro <94948678+DarkMattrMaestro@users.noreply.github.com>
2026-02-17 01:58:02 -0500

    Update to CR 0.5.16 and add `data` and `repeat` commands

--- src/client/java/com/darkmattrmaestro/tick_manipulator/commands/CommandTargetData.java
+++ src/client/java/com/darkmattrmaestro/tick_manipulator/commands/CommandTargetData.java
@@ -2,9 +2,11 @@ package com.darkmattrmaestro.tick_manipulator.commands;
 
 import com.darkmattrmaestro.tick_manipulator.Constants;
 import com.darkmattrmaestro.tick_manipulator.utils.BlockSelectionUtil;
+import com.darkmattrmaestro.tick_manipulator.utils.EntitySelectionUtil;
 import finalforeach.cosmicreach.blocks.BlockPosition;
 import finalforeach.cosmicreach.chat.IChat;
 import finalforeach.cosmicreach.chat.commands.Command;
+import finalforeach.cosmicreach.entities.Entity;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -32,15 +34,12 @@ public class CommandTargetData extends Command {
                 }
             }
             for (Method method: clazz.getDeclaredMethods()) {
-                if (method.getParameterCount() != 0 || method.getReturnType().equals(Void.TYPE)) {
-                    continue;
-                }
+                if (method.getParameterCount() != 0 || method.getReturnType().equals(Void.TYPE)) { continue; }
 
                 try {
+                    method.setAccessible(true);
                     msg.append(String.format("\n%s%s -> %s", getIndent(indentLevel + 1), method.getName(), method.invoke(obj)));
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                } catch (InvocationTargetException e) {
+                } catch (IllegalAccessException | InvocationTargetException e) {
                     throw new RuntimeException(e);
                 }
             }
@@ -52,22 +51,74 @@ public class CommandTargetData extends Command {
         return msg.toString();
     }
 
+    public <T> String logData(T obj, int indentLevel, String targetPropertyName) {
+        StringBuilder msg = new StringBuilder();
+        Class clazz = obj.getClass();
+        while (clazz != Object.class) {
+            msg.append(String.format("\n%s%s : {", getIndent(indentLevel), clazz.getName()));
+            for (Field field: clazz.getDeclaredFields()) {
+                if (targetPropertyName != null && !field.getName().equals(targetPropertyName)) { continue; }
+
+                try {
+                    field.setAccessible(true);
+                    msg.append(String.format("\n%s%s : %s", getIndent(indentLevel + 1), field.getName(), field.get(obj)));
+                } catch (IllegalAccessException e) {
+                    msg.append(String.format("\n%s%s :\\ No access to field!", getIndent(indentLevel + 1), field.getName()));
+                }
+            }
+            for (Method method: clazz.getDeclaredMethods()) {
+                if (targetPropertyName != null && !method.getName().equals(targetPropertyName)) { continue; }
+                if (method.getParameterCount() != 0 || method.getReturnType().equals(Void.TYPE)) { continue; }
+
+                try {
+                    method.setAccessible(true);
+                    msg.append(String.format("\n%s%s -> %s", getIndent(indentLevel + 1), method.getName(), method.invoke(obj)));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            msg.append(String.format("\n%s}", getIndent(indentLevel)));
+
+            clazz = clazz.getSuperclass();
+        }
+
+        return msg.toString();
+    }
+
+    public <T> void logDataOf(T obj, String name, String targetPropertyName) {
+        if (obj == null) {
+            Constants.LOGGER.info("No {} found!", name);
+            sendMsg("No " + name + " found!");
+            return;
+        }
+
+        String logDataStr;
+        if (targetPropertyName != null) { logDataStr = logData(obj, 1, targetPropertyName); }
+        else { logDataStr = logData(obj, 1); }
+
+        Constants.LOGGER.info(" \nData of {} :\n{}\n}", obj, logDataStr);
+        sendMsg(String.format("\nData of %s :\n%s\n}", obj, logDataStr));
+    }
+
     public void run(IChat chat) {
         super.run(chat);
 
         if (!this.hasNextArg()) {
-            BlockPosition blockPos = BlockSelectionUtil.getBlockLookingAtFar(100);
-            if (blockPos == null) {
-                Constants.LOGGER.info("No block found!");
-                sendMsg("No block found!");
-                return;
+            logDataOf(BlockSelectionUtil.getBlockLookingAtFar(100), "block", null);
+        } else {
+            String targetType = this.getNextArg();
+            String targetPropertyName = null;
+            if (this.hasNextArg()) {
+                targetPropertyName = this.getNextArg().toLowerCase();
             }
 
-            String logDataStr = logData(blockPos, 1);
-            Constants.LOGGER.info(" \nData of {} :\n{}\n}", blockPos, logDataStr);
-            sendMsg(String.format("\nData of %s :\n%s\n}", blockPos, logDataStr));
-        } else {
-            sendMsg("Invalid command arguments.");
+            if ("block".equals(targetType)) {
+                logDataOf(BlockSelectionUtil.getBlockLookingAtFar(100), "block", targetPropertyName);
+            } else if ("entity".equals(targetType)) {
+                logDataOf(EntitySelectionUtil.getNearestEntityToPlayer(), "entity", targetPropertyName);
+            } else {
+                sendMsg("Invalid command arguments.");
+            }
         }
     }
 
---
+++ src/client/java/com/darkmattrmaestro/tick_manipulator/utils/EntitySelectionUtil.java
@@ -0,0 +1,29 @@
+package com.darkmattrmaestro.tick_manipulator.utils;
+
+import com.badlogic.gdx.math.Vector3;
+import com.darkmattrmaestro.tick_manipulator.Constants;
+import finalforeach.cosmicreach.entities.Entity;
+import finalforeach.cosmicreach.gamestates.InGame;
+import finalforeach.cosmicreach.util.ArrayUtils;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class EntitySelectionUtil {
+    public static Entity getNearestEntityToPlayer() {
+        Vector3 playerPos = InGame.getLocalPlayer().getPosition();
+        AtomicReference<Entity> entity = new AtomicReference<>();
+        AtomicReference<Float> closest = new AtomicReference<Float>(-1f);
+        ArrayUtils.forEach(InGame.getLocalPlayer().getZone().getAllEntities().toArray(Entity.class), (Entity e) -> {
+            if ("base:entity_player".equals(e.entityTypeId)) { return; }
+
+            float dist2 = e.position.dst2(playerPos);
+            Constants.LOGGER.warn("e type: {}", e.entityTypeId);
+            if (closest.get() < 0 || dist2 < closest.get()) {
+                closest.set(dist2);
+                entity.set(e);
+            }
+        });
+
+        return entity.get();
+    }
+}
