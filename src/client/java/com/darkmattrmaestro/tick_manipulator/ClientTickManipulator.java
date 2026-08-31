32b50166ba87504fbe558cc9304db51391b992f2
DarkMattrMaestro <94948678+DarkMattrMaestro@users.noreply.github.com>
2026-08-31 00:30:47 -0400

    Remove unused Dear ImGui windows

--- src/client/java/com/darkmattrmaestro/tick_manipulator/ClientTickManipulator.java
+++ src/client/java/com/darkmattrmaestro/tick_manipulator/ClientTickManipulator.java
@@ -2,11 +2,7 @@ package com.darkmattrmaestro.tick_manipulator;
 
 import com.darkmattrmaestro.tick_manipulator.commands.CommandHighlight;
 import com.darkmattrmaestro.tick_manipulator.commands.CommandTargetData;
-import com.darkmattrmaestro.tick_manipulator.imguiStuff.HighlightWindow;
-import com.darkmattrmaestro.tick_manipulator.imguiStuff.LaunchpadWindow;
-import com.darkmattrmaestro.tick_manipulator.imguiStuff.TestWindow;
 import dev.puzzleshq.puzzleloader.loader.mod.entrypoint.client.ClientModInit;
-import org.tympanic.imgui_integration.imgui.ImGuiManager;
 
 import static finalforeach.cosmicreach.chat.commands.Command.registerCommand;
 
@@ -18,8 +14,6 @@ public class ClientTickManipulator implements ClientModInit {
         Constants.LOGGER.info("Initialized Tick Manipulator Client");
         registerCommand(CommandHighlight::new, "highlight", "hl");
         registerCommand(CommandTargetData::new, "target-data", "data");
-
-        Constants.clientTickGUISpawner = () -> ImGuiManager.INSTANCE.windows.add(new LaunchpadWindow());
     }
 
 }
